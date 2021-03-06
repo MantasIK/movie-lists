7de068c5e18950a9c007334847df264d35c87c6b
Mantas Ivaskevicius <mantasivaskevicius@gmail.com>
2021-03-05 19:08:33 -0500

    Added shcemas

--- server/database/index.js
+++ server/database/index.js
@@ -1,18 +1,30 @@
-// 
-
-const mongoose = require('mongoose');
-if(process.env.MONGODB_URI){
-  mongoose.connect(process.env.MONGODB_URI)
-} else{
-  mongoose.connect('mongodb://localhost:27017/badmovies', { useNewUrlParser: true });
+const mongoose = require("mongoose");
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost:27017/badmovies", {
+    useNewUrlParser: true,
+  });
 }
 
 const db = mongoose.connection;
 
 mongoose.Promise = Promise;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to db...');
-})
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Connected to db...");
+});
 
-module.exports.db = db
+const loveSchema = new mongoose.Schema({
+  original_title: { type: String, unique: true },
+  poster_path: String,
+  popularity: Number,
+  release_date: String,
+});
+
+const hateSchema = new mongoose.Schema({
+  original_title: { type: String, unique: true },
+  poster_path: String,
+  popularity: Number,
+  release_date: String,
+});
--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
+const apiKey = "";
 
-module.exports.getGenres = function() {
+module.exports.getGenres = function () {
   return axios.get(
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=0c00aa7a4d2e50f65c74d3176f797345&language=en-US"
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
   );
 };
 
-module.exports.getMovies = function(id) {
+module.exports.getMovies = function (id) {
   return axios.get(
-    `https://api.themoviedb.org/3/discover/movie?api_key=0c00aa7a4d2e50f65c74d3176f797345&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=100&with_genres=${id}`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=100&with_genres=${id}`
   );
 };
